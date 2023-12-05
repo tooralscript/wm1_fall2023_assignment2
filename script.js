89217b34f0276ae29acbbd98773da67622836fb0
tooralscript <thasanov15638@ada.edu.az>
2023-12-05 11:17:21 +0400

    loading spinner added

--- script.js
+++ script.js
@@ -1,18 +1,26 @@
 let products = {};
 let productsSection = document.querySelector("section");
+let dataLoading = document.querySelector(".circles");
+
+setTimeout(() => {
+  dataLoading.style.display = "none";
+}, 4000);
 
 fetch("https://dummyjson.com/products")
   .then((response) => response.json())
   .then((data) => {
     console.log(data);
-    displayData(data);
-  });
+    setTimeout(() => {
+      displayData(data);
+    }, 4000);
+  })
+  .catch((error) => console.log(error));
 
 displayData = (data) => {
   for (let i = 0; i < data.limit; i++) {
     console.log("for ishe dushdu");
     productsSection.innerHTML += `
-    <div class="product">
+    <div class="product" >
       <img src="${data.products[i].thumbnail}" alt="">
       <span class="productTitle">${data.products[i].title}</span>
       <span>${data.products[i].discountPercentage}% discount!</span>
@@ -21,4 +29,9 @@ displayData = (data) => {
     </div>
     `;
   }
+};
+
+displayError = () => {
+  dataLoading.style.display = "none";
+  productsSection.innerHTML += `<span class="errorMessage">Sorry, something went wrong...</span>`;
 };
